abbcc69afd8d243784048af1b90f9e74eea833c7
Felipe Almeida <almeida.felipe98@gmail.com>
2026-01-09 20:40:10 -0300

    add content negotiation via Header Param

--- rest-with-spring-boot/src/main/java/com/felipelalmeida/config/WebConfig.java
+++ rest-with-spring-boot/src/main/java/com/felipelalmeida/config/WebConfig.java
@@ -10,9 +10,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+        // via QUERY PARAM
+//        configurer.favorParameter(true)
+//                .parameterName("mediaType")
+//                .ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+
+        // via HEADER PARAM
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
