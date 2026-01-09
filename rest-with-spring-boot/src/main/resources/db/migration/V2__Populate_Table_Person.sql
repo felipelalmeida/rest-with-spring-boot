5b5f63f89f75c2648a6334eecd439fa7ca661fee
Felipe Almeida <almeida.felipe98@gmail.com>
2026-01-09 19:52:29 -0300

    add migration with flyway

---
+++ rest-with-spring-boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,11 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+	(3, 'São Paulo - Brasil', 'Gabriel', 'Male', 'Bortoleto'),
+	(4, 'Bristol - United Kingdom', 'Lando', 'Male', 'Norris'),
+	(5, 'Melbourne - Australia', 'Oscar', 'Male', 'Piastri'),
+	(6, 'Hasselt - Netherlands', 'Max', 'Male', 'Verstappen'),
+	(7, 'Paris - France', 'Isack', 'Male', 'Hadjar'),
+	(9, 'Nastola - Finland', 'Valtteri', 'Male', 'Bottas'),
+	(10, 'Monte Carlo - Monaco', 'Charles', 'Male', 'Leclerc'),
+	(12, 'Évreux - France', 'Esteban', 'Male', 'Ocon'),
+	(13, 'Évreux - France', 'Esteban', 'Male', 'Ocon');
