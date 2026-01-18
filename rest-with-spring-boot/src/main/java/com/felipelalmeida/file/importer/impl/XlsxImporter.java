7b703e012d236a9abcb454cdfee19b8842907472
Felipe Almeida <almeida.felipe98@gmail.com>
2026-01-18 14:54:17 -0300

    add initial Importer config

---
+++ rest-with-spring-boot/src/main/java/com/felipelalmeida/file/importer/impl/XlsxImporter.java
@@ -0,0 +1,14 @@
+package com.felipelalmeida.file.importer.impl;
+
+import com.felipelalmeida.data.dto.PersonDTO;
+import com.felipelalmeida.file.importer.contract.FileImporter;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class XlsxImporter implements FileImporter {
+    @Override
+    public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
+        return List.of();
+    }
+}
