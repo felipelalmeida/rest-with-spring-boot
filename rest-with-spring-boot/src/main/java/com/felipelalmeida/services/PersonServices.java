a1badf017c71e1e1691906f1ad1ed57b71a3d4c6
Felipe Almeida <almeida.felipe98@gmail.com>
2026-01-18 15:22:09 -0300

    organize PersonService

--- rest-with-spring-boot/src/main/java/com/felipelalmeida/services/PersonServices.java
+++ rest-with-spring-boot/src/main/java/com/felipelalmeida/services/PersonServices.java
@@ -10,6 +10,7 @@ import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
@@ -35,48 +36,14 @@ public class PersonServices {
 
     public PagedModel<EntityModel<PersonDTO>> findAll(Pageable pageable){
         logger.info("Find all people!");
-
         var people = repository.findAll(pageable);
-        var peopleWithLinks = people.map(person -> {
-            var dto = parseObject(person, PersonDTO.class);
-            addHateoasLinks(dto);
-            return dto;
-        });
-
-        Link findAllLinks = WebMvcLinkBuilder.linkTo(
-                WebMvcLinkBuilder.methodOn(PersonController.class)
-                        .findAll(
-                                pageable.getPageNumber(),
-                                pageable.getPageSize(),
-                                String.valueOf(pageable.getSort())
-                        )
-        ).withSelfRel();
-
-        return assembler.toModel(peopleWithLinks,
-                findAllLinks);
+        return buildPagedModel(pageable, people);
     }
 
     public PagedModel<EntityModel<PersonDTO>> findPeopleByName(String firstName, Pageable pageable){
         logger.info("Find people by name!");
-
         var people = repository.findPeopleByName(firstName, pageable);
-        var peopleWithLinks = people.map(person -> {
-            var dto = parseObject(person, PersonDTO.class);
-            addHateoasLinks(dto);
-            return dto;
-        });
-
-        Link findAllLinks = WebMvcLinkBuilder.linkTo(
-                WebMvcLinkBuilder.methodOn(PersonController.class)
-                        .findAll(
-                                pageable.getPageNumber(),
-                                pageable.getPageSize(),
-                                String.valueOf(pageable.getSort())
-                        )
-        ).withSelfRel();
-
-        return assembler.toModel(peopleWithLinks,
-                findAllLinks);
+        return buildPagedModel(pageable, people);
     }
 
     public PersonDTO findById(Long id){
@@ -89,7 +56,6 @@ public class PersonServices {
 
     public PersonDTO create(PersonDTO person) {
         if (person == null) throw new RequiredObjectIsNullException();
-
         logger.info("Creating one Person!");
         var entity = parseObject(person, Person.class);
         var dto = parseObject(repository.save(entity), PersonDTO.class);
@@ -99,11 +65,9 @@ public class PersonServices {
 
     public PersonDTO update(PersonDTO person) {
         if (person == null) throw new RequiredObjectIsNullException();
-
         logger.info("Updating one Person!");
 
         Person entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
-
         entity.setFirstName(person.getFirstName());
         entity.setLastName(person.getLastName());
         entity.setAddress(person.getAddress());
@@ -118,14 +82,12 @@ public class PersonServices {
         logger.info("Deleting one Person!");
 
         Person entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
-
         repository.delete(entity);
     }
 
     @Transactional
     public PersonDTO disablePerson(Long id){
         logger.info("Disabling one Person!");
-
         repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
         repository.disablePerson(id);
         var entity = repository.findById(id).get();
@@ -134,9 +96,29 @@ public class PersonServices {
         return dto;
     }
 
+    private PagedModel<EntityModel<PersonDTO>> buildPagedModel(Pageable pageable, Page<Person> people) {
+        var peopleWithLinks = people.map(person -> {
+            var dto = parseObject(person, PersonDTO.class);
+            addHateoasLinks(dto);
+            return dto;
+        });
+
+        Link findAllLinks = WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(PersonController.class)
+                        .findAll(
+                                pageable.getPageNumber(),
+                                pageable.getPageSize(),
+                                String.valueOf(pageable.getSort())
+                        )
+        ).withSelfRel();
+
+        return assembler.toModel(peopleWithLinks, findAllLinks);
+    }
+
     private void addHateoasLinks(PersonDTO dto) {
         dto.add(linkTo(methodOn(PersonController.class).findById(dto.getId())).withSelfRel().withType("GET"));
         dto.add(linkTo(methodOn(PersonController.class).findAll(1,12, "asc")).withRel("findAll").withType("GET"));
+        dto.add(linkTo(methodOn(PersonController.class).findPeopleByName("",1,12, "asc")).withRel("findPeopleByName").withType("GET"));
         dto.add(linkTo(methodOn(PersonController.class).create(dto)).withRel("create").withType("POST"));
         dto.add(linkTo(methodOn(PersonController.class).disablePerson(dto.getId())).withRel("disable").withType("PATCH"));
         dto.add(linkTo(methodOn(PersonController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
