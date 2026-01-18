c94f9ad8dd8974d3e0fb7d9b644079bcab54fcf9
Felipe Almeida <almeida.felipe98@gmail.com>
2026-01-18 12:26:36 -0300

    add downlaodFile controller

--- rest-with-spring-boot/src/main/java/com/felipelalmeida/controllers/docs/FileControllerDocs.java
+++ rest-with-spring-boot/src/main/java/com/felipelalmeida/controllers/docs/FileControllerDocs.java
@@ -3,6 +3,7 @@ package com.felipelalmeida.controllers.docs;
 import com.felipelalmeida.data.dto.UploadFileResponseDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,5 +14,5 @@ public interface FileControllerDocs {
 
     UploadFileResponseDTO uploadFile(MultipartFile file);
     List<UploadFileResponseDTO> uploadMultipleFiles(MultipartFile[] files);
-    ResponseEntity<ResponseEntity> downloadFile(String fileName, HttpServletRequest request);
+    ResponseEntity<Resource> downloadFile(String fileName, HttpServletRequest request);
 }
