8d71fb334053d72069b532a5ae8b77b20eb0f1ca
Felipe Almeida <almeida.felipe98@gmail.com>
2026-01-20 09:58:34 -0300

    user Service and Repository implementation

---
+++ rest-with-spring-boot/src/main/java/com/felipelalmeida/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.felipelalmeida.repository;
+
+import com.felipelalmeida.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.userName =:userName")
+    User findByUserName(@Param("userName") String userName);
+}
