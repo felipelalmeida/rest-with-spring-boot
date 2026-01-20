d76d1a3f8d739bb22dd5c7a51928785c3a21f02d
Felipe Almeida <almeida.felipe98@gmail.com>
2026-01-20 10:54:24 -0300

    tokenProvider and DTOs implementation

---
+++ rest-with-spring-boot/src/main/java/com/felipelalmeida/data/dto/security/AccountCredentialsDTO.java
@@ -0,0 +1,43 @@
+package com.felipelalmeida.data.dto.security;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class AccountCredentialsDTO implements Serializable {
+
+    private final static long serialVersionUID = 1L;
+
+    private String userName;
+    private String password;
+
+    public AccountCredentialsDTO() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountCredentialsDTO that = (AccountCredentialsDTO) o;
+        return Objects.equals(getUserName(), that.getUserName()) && Objects.equals(getPassword(), that.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserName(), getPassword());
+    }
+}
---
+++ rest-with-spring-boot/src/main/java/com/felipelalmeida/data/dto/security/TokenDTO.java
@@ -0,0 +1,89 @@
+package com.felipelalmeida.data.dto.security;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class TokenDTO implements Serializable {
+
+    private final static long serialVersionUID = 1L;
+
+    private String userName;
+    private Boolean authenticated;
+    private Date created;
+    private Date expiration;
+    private String accessToken;
+    private String refreshToken;
+
+    public TokenDTO() {
+    }
+
+    public TokenDTO(String userName, Boolean authenticated, Date created, Date expiration, String accessToken, String refreshToken) {
+        this.userName = userName;
+        this.authenticated = authenticated;
+        this.created = created;
+        this.expiration = expiration;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Boolean getAuthenticated() {
+        return authenticated;
+    }
+
+    public void setAuthenticated(Boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        TokenDTO tokenDTO = (TokenDTO) o;
+        return Objects.equals(getUserName(), tokenDTO.getUserName()) && Objects.equals(getAuthenticated(), tokenDTO.getAuthenticated()) && Objects.equals(getCreated(), tokenDTO.getCreated()) && Objects.equals(getExpiration(), tokenDTO.getExpiration()) && Objects.equals(getAccessToken(), tokenDTO.getAccessToken()) && Objects.equals(getRefreshToken(), tokenDTO.getRefreshToken());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserName(), getAuthenticated(), getCreated(), getExpiration(), getAccessToken(), getRefreshToken());
+    }
+}
