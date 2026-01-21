e5755c16585d05cce1af0e45e1e88d71e531a148
Felipe Almeida <almeida.felipe98@gmail.com>
2026-01-21 10:04:45 -0300

    authenticationManager and securityFilterChain implementation in securityConfig

--- rest-with-spring-boot/src/main/java/com/felipelalmeida/config/SecurityConfig.java
+++ rest-with-spring-boot/src/main/java/com/felipelalmeida/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.felipelalmeida.config;
 
+import com.felipelalmeida.security.JwtTokenFilter;
 import com.felipelalmeida.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,4 +46,34 @@ public class SecurityConfig {
         return passwordEncoder;
     }
 
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtTokenFilter filter = new JwtTokenFilter(tokenProvider);
+        //@formatter:off
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests.requestMatchers(
+                                "/auth/signin",
+                                "/auth/refresh/**",
+                                "/auth/createUser",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                                .requestMatchers("/api/**").authenticated()
+                                .requestMatchers("/users").denyAll()
+                )
+                .cors(cors -> {})
+                .build();
+        //@formater:on
+    }
 }
