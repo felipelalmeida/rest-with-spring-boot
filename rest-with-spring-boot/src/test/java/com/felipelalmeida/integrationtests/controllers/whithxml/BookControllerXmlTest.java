089a3f8adcb6d269e4af3b51b4423061ba8aa11a
Felipe Almeida <almeida.felipe98@gmail.com>
2026-01-23 17:43:16 -0300

    update BookControllers test

--- rest-with-spring-boot/src/test/java/com/felipelalmeida/integrationtests/controllers/whithxml/BookControllerXmlTest.java
+++ rest-with-spring-boot/src/test/java/com/felipelalmeida/integrationtests/controllers/whithxml/BookControllerXmlTest.java
@@ -1,7 +1,9 @@
 package com.felipelalmeida.integrationtests.controllers.whithxml;
 
 import com.felipelalmeida.config.TestConfigs;
+import com.felipelalmeida.integrationtests.dto.AccountCredentialsDTO;
 import com.felipelalmeida.integrationtests.dto.BookDTO;
+import com.felipelalmeida.integrationtests.dto.TokenDTO;
 import com.felipelalmeida.integrationtests.dto.wrappers.xmlandyaml.PagedModelBook;
 import com.felipelalmeida.integrationtests.testcontainers.AbstractIntegrationTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -29,6 +31,7 @@ class BookControllerXmlTest extends AbstractIntegrationTest {
     private static XmlMapper objectMapper;
 
     private static BookDTO book;
+    private static TokenDTO tokenDTO;
 
     @BeforeAll
     static void setUp() {
@@ -36,20 +39,44 @@ class BookControllerXmlTest extends AbstractIntegrationTest {
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         book = new BookDTO();
+        tokenDTO = new TokenDTO();
+    }
+
+    @Test
+    @Order(0)
+    void signin() {
+        AccountCredentialsDTO credentials = new AccountCredentialsDTO("leandro", "admin123");
+
+        tokenDTO = given()
+                .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(credentials)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenDTO.class);
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_LOCAL)
+                .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenDTO.getAccessToken())
+                .setBasePath("/api/book/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        assertNotNull(tokenDTO.getAccessToken());
+        assertNotNull(tokenDTO.getRefreshToken());
     }
 
     @Test
     @Order(1)
     void createTest() throws JsonProcessingException {
         mockBook();
-
-        specification = new RequestSpecBuilder()
-            .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
-            .setBasePath("/api/book/v1")
-            .setPort(TestConfigs.SERVER_PORT)
-                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
-                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
-            .build();
 
         var content = given(specification)
             .contentType(MediaType.APPLICATION_XML_VALUE)
@@ -152,11 +179,13 @@ class BookControllerXmlTest extends AbstractIntegrationTest {
 
         specification = new RequestSpecBuilder()
                 .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
+                .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenDTO.getAccessToken())
                 .setBasePath("/api/book/v1/all")
                 .setPort(TestConfigs.SERVER_PORT)
                 .addFilter(new RequestLoggingFilter(LogDetail.ALL))
                 .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
                 .build();
+
 
         var content = given(specification)
                 .accept(MediaType.APPLICATION_XML_VALUE)
